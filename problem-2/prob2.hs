7f67e0a77e17c44ee1b28e2eda2552f69708f073
dechiad1 <dechiad1@tcnj.edu>
2016-11-05 15:28:29 -0400

    solution for problem 2 of project euhler

---
+++ problem-2/prob2.hs
@@ -0,0 +1,11 @@
+{-!
+ Project Euler problem 2. Add all even fibonacci numbers less than 4000000
+-}
+
+main = print(sumEvenFibsLessThan 4000000)
+
+--sumEvenFibsLessThan :: (Integer a) => a -> a
+sumEvenFibsLessThan n = sum (filter even (takeWhile (<n) fibs))
+
+fibs = 1:1:zipWith (+) fibs (tail fibs)
+
